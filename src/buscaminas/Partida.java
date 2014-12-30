abd2361a320fdcc876d9cedeb8b9a20189a2392c
alexmorral <alexmorral@gmail.com>
2014-12-30 20:31:31 +0100

    Partida y nivel working + DriverPartida

--- src/buscaminas/Partida.java
+++ src/buscaminas/Partida.java
@@ -5,13 +5,16 @@
  */
 package buscaminas;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import java.util.Set;
 
 /**
  *
@@ -27,7 +30,7 @@ public class Partida {
     private int nombreTirades;
     
     @OneToMany private Nivell nivell;
-    
+    private Casella[][] caselles;
 
     public Partida(int idPartida) {
         this.idPartida=idPartida;
@@ -35,7 +38,43 @@ public class Partida {
         this.estaGuanyada=false;
         this.nombreTirades=0;
     }
-
+    
+    
+    /**
+     * @return ArrayList<String> amb posicio de les mines
+     */
+    private ArrayList<String> generarMinas() {
+        int nombreMines = nivell.getNombreMines();
+        ArrayList<Integer> f = new ArrayList<>();
+        for(int i = 0; i < nivell.getNombreCasellesxFila(); ++i) f.add(i);
+        ArrayList<Integer> c = new ArrayList<>();
+        for(int i = 0; i < nivell.getNombreCasellesxColumna(); ++i) c.add(i);
+        int count = 0;
+        ArrayList<String> minas = new ArrayList<>();
+        while(count < nombreMines) {
+            Collections.shuffle(f);
+            Collections.shuffle(c);
+            if(!minas.contains(f.get(0)+" "+c.get(0))) minas.add(f.get(0)+" "+c.get(0));
+            ++count;
+        }
+        return minas;
+    }
+    
+    
+    public void initPartida() {    
+        this.caselles = new Casella[nivell.getNombreCasellesxFila()][nivell.getNombreCasellesxColumna()];
+        ArrayList<String> minas = generarMinas();
+        for(int i = 0; i < nivell.getNombreCasellesxFila(); ++i) {
+            for(int j = 0; j < nivell.getNombreCasellesxColumna(); ++j) {
+                boolean mina = false;
+                for(String st : minas) {
+                    String[] parts = st.split(" ");
+                    if(Integer.parseInt(parts[0])==i && Integer.parseInt(parts[1])==j) mina = true;
+                }
+                caselles[i][j] = new Casella(i, j, false, false, mina);
+            }
+        }
+    }
     
     /**
      * @return the idPartida
@@ -93,5 +132,22 @@ public class Partida {
         this.nombreTirades = nombreTirades;
     }
     
+    /**
+     * @param nivell the nivell to set
+     */
+    public void setNivell(Nivell nivell) {
+        this.nivell = nivell;
+    }
+    
+    
+    public void mostrarPartida() {
+        for(int i = 0; i < nivell.getNombreCasellesxFila(); ++i) {
+            for(int j = 0; j < nivell.getNombreCasellesxColumna(); ++j) {
+                if(caselles[i][j].getTeMina()) System.out.printf("* ");
+                else System.out.printf(". ");
+            }
+            System.out.println("");
+        }
+    }
     
 }
